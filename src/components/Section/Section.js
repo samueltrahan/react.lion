ff73c472e59929799c9510d99b5f7378434b466a
Samuel Trahan <samtrahan91@gmail.com>
2020-11-07 12:22:07 -0600

    section styling almost complete

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './Section.css';
 
 export default function Section() {
   return (
     <div className="section">
-      <section>
-        <h1>Instagram</h1>
+      <Link to="/instagram">
+      <section className="instagram">
+        <h1 className="section-head">Instagram</h1>
       </section>
-      <section>
-        <h1>CWD</h1>
+      </Link>
+      <Link target="_blank" to="https://www.cinquewd.com">
+      <section className="cinque">
+        <h1 className="section-head">CWD</h1>
       </section>
-      <section>
-        <h1>Extreme Neon</h1>
+      </Link>
+      <Link to="/neon">
+      <section className="neon">
+        <h1 className="section-head">Extreme Neon</h1>
       </section>
-      <section>
-        <h1>Breakfast</h1>
+      </Link>
+      <Link to="/breakfast">
+      <section className="breakfast">
+        <h1 className="section-head">Breakfast</h1>
       </section>
+      </Link>
     </div>
   )
 }
