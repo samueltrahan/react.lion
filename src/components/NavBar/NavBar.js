e06be1beb9ddf1435a6dd78ece8f08dd91bca20f
Samuel Trahan <samtrahan91@gmail.com>
2020-11-07 11:40:51 -0600

    nav bar styling complete

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './NavBar.css';
+import Pic01 from '../../Assets/lion.png';
 
 export default function NavBar() {
   return (
-    <div>
-      NavBar
+    <div className="nav-bar">
+      <div className="logo">
+      <img className="lion-logo" alt="" src={Pic01}></img>
+      </div>
+      <div className="links">
+        <a className="nav-links" href="/projects">Projects</a>
+        <a className="nav-links" href="/instagram">Instagram</a>
+        <a className="nav-links" href="/about">About</a>
+      </div>
     </div>
   )
 }
