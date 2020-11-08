f843a67ad694fec6257407c55eb1ffde44f97c9f
Samuel Trahan <samtrahan91@gmail.com>
2020-11-08 14:28:21 -0600

    footer styling and set up

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Banner from './components/Banner/Banner';
 import Section from './components/Section/Section';
+import Footer from './components/Footer/Footer';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
      <NavBar />
      <Banner />
      <Section />
+     <Footer />
     </Router>
   )
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>&copy; 2020 by Samuel </p>
+      <p><a className="footer-links" href="https://github.com/samueltrahan/react.lion">Theme</a> by <a className="footer-links" href="https://www.cinquewd.com">Cinque Web Development</a></p>
+    </div>
+  )
+}
